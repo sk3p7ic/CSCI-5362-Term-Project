38c0fd2b19f512c86ee2278ac7e80006fc018962
Joshua Ibrom <joshuaibrom@gmail.com>
2025-04-29 09:37:33 -0500

    Add CWE samples

---
+++ samples/cwe-787.c
@@ -0,0 +1,14 @@
+/**
+ * Source: https://cwe.mitre.org/data/definitions/787.html
+ */
+
+int main() {
+  int id_sequence[3];
+
+  /* Populate the id array. */
+
+  id_sequence[0] = 123;
+  id_sequence[1] = 234;
+  id_sequence[2] = 345;
+  id_sequence[3] = 456;
+}
